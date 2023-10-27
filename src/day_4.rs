1f42529f37fb2460ec399b8848c4e72b43a7fc09
Mason Fisher <masonfisherb@gmail.com>
2023-10-27 09:59:50 -0500

    day4

--- src/day_4.rs
+++ src/day_4.rs
@@ -22,7 +22,29 @@ pub fn part_1() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 pub fn part_2() -> Result<(), Box<dyn std::error::Error>> {
-    let solution = "0".to_string();
+    let solution = std::fs::read_to_string("input/day_4.prod")?
+        .lines()
+        .map(|line|  line                            
+                .split(',')
+                .map(|range| {
+                    let mut x = range
+                        .split('-')
+                        .map(|x| x.parse::<i32>().unwrap()) ;
+                    (x.next().unwrap(), x.next().unwrap())
+                }).collect::<Vec<(i32, i32)>>()
+            .chunks(2)
+            .map(|x| {
+                let (min1,max1) = if x[0].0 > x[0].1 { (x[0].1,x[0].0) } else { x[0] };
+                let (min2,max2) = if x[1].0 > x[1].1 { (x[1].1,x[1].0) } else { x[1] };
+                
+                // 1-1, 1-2
+                (min1 >= min2 && min1 <= max2)      // min1 is in range2
+                || (max1 >= min2 && max1 <= max2)   // max1 is in range2
+                || (min2 >= min1 && min2 <= max1)   // min2 is in range1
+                || (max2 >= min1 && max2 <= max1)   // max2 is in range1
+            }).next().unwrap()
+        ).filter(|x| *x).count();
+        
     println!("{}", solution);
     Ok(())
 }
